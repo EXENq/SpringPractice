82eca45c012c07c7e5cf3c15c31095663111a0bb
exen <exen@DESKTOP-EPC0LM3>
2020-09-14 21:23:02 +0400

    commit

--- src/main/java/ru/exen/controller/MainController.java
+++ src/main/java/ru/exen/controller/MainController.java
@@ -3,6 +3,7 @@ package ru.exen.controller;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ import ru.exen.repo.MessageRepo;
 public class MainController {
 	private final MessageRepo messageRepo;
 	
+	@Value("${spring.profiles.active}")
+	private String profile;
+	
 	@Autowired
 	public MainController(MessageRepo messageRepo) {
 		this.messageRepo = messageRepo;
@@ -30,6 +34,8 @@ public class MainController {
 		data.put("messages", messageRepo.findAll());
 		
 		model.addAttribute("frontendData", data);
+		model.addAttribute("isDevMode", "dev".equals(profile));
+		
 		return "index";
 	}
 }
